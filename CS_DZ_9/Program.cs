3abb9d4434ab953fb8c4c1c10a08a55ad9e4e4a4
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-09 09:24:11 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i < 100;  i += 7) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
